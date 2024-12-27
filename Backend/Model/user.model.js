3e7cd1ae8c844e35ffe018253b7cd8a2f15f53ab
khalid chouhan <khalidchuhan7886@gmail.com>
2024-12-27 22:05:56 +0500

    creating Model

---
+++ Backend/Model/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userScema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const user = mongoose.model("User", userScema);
+export default user;
